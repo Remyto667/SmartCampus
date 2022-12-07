ed9e484b92303cfc0c557046c703cb0731556086
jmoulia <jmoulia@etudiant.univ-lr.fr>
2022-12-07 12:06:41 +0100

    ajout des alertes dans une nouvelle page + alerte sur la page affichant les donnés

--- symfony/src/Controller/RoomController.php
+++ symfony/src/Controller/RoomController.php
@@ -25,4 +25,22 @@ class RoomController extends AbstractController
 
     }
 
+    #[Route('/salle/alerte/{id?}', name: 'alerte')]
+    public function alerte( ?int $id): Response
+    {
+        $json = '../assets/json/dataRoom.json'; // chemin d'accès à votre fichier JSON
+        $file = file_get_contents($json); // mettre le contenu du fichier dans une variable
+        $obj = json_decode($file); // décoder le flux JSON
+
+        return $this->render('room/alerte.html.twig', [
+            'id' => $id,
+            'obj' => $obj,
+            'room' => $obj[0]->{"localisation"},
+            'temp' => $obj[0]->{"valeur"},
+            'hum' => $obj[1]->{"valeur"},
+            'co2' => $obj[2]->{"valeur"},
+        ]);
+
+    }
+
 }
