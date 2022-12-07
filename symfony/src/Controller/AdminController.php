3e39c022a1a4c6a20764077869d29fbbb208d1fb
Amandine Berger <amandine.berger@etudiant.univ-lr.fr>
2022-12-07 12:01:38 +0100

    correction des routes pour l'affichage des donnees de salle

--- symfony/src/Controller/AdminController.php
+++ symfony/src/Controller/AdminController.php
@@ -238,22 +238,5 @@ class AdminController extends AbstractController
         return $this->redirect($this->generateUrl('listerSalles', ['ok' => $repository->remove($room, true)]));
     }
 
-    #[Route('/salle/{name?}', name: 'donneesSalle')]
-    public function donnees_salle(Request $request, ?string $name, ManagerRegistry $doctrine): Response{
-        $entityManager = $doctrine->getManager();
-        $repository = $entityManager->getRepository('App\Entity\Room');
-        $room = $repository->findRoomByName($name);
-
-        $json = "../assets/json/".$room->getName().".json";
-        $file = file_get_contents($json);
-        $obj = json_decode($file);
-
-        return $this->render('admin/donnees_salle.html.twig', [
-            'obj' => $obj,
-            'room' => $obj[0]->{"localisation"},
-            'temp' => $obj[0]->{"valeur"},
-            'hum' => $obj[1]->{"valeur"},
-            'co2' => $obj[2]->{"valeur"},
-        ]);    }
 }
 
