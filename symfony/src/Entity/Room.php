32993ee4f6b9568f582519fda85088da4b00011d
cbonnard <cyril.bonnard@etudiant.univ-lr.fr>
2022-12-12 14:42:07 +0100

    commit pour récup taf

--- symfony/src/Entity/Room.php
+++ symfony/src/Entity/Room.php
@@ -28,6 +28,21 @@ class Room
     #[ORM\Column(nullable: true)]
     private ?bool $isStock = null;
 
+    #[ORM\Column]
+    private ?int $room_size = null;
+
+    #[ORM\Column]
+    private ?int $windows_number = null;
+
+    #[ORM\Column]
+    private ?int $type = null;
+
+    #[ORM\Column(length: 3)]
+    private ?string $orientation = null;
+
+    #[ORM\Column]
+    private ?int $floor = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -73,6 +88,66 @@ class Room
     public function setIsStock(?bool $isStock): self
     {
         $this->isStock = $isStock;
+
+        return $this;
+    }
+
+    public function getRoomSize(): ?int
+    {
+        return $this->room_size;
+    }
+
+    public function setRoomSize(int $room_size): self
+    {
+        $this->room_size = $room_size;
+
+        return $this;
+    }
+
+    public function getWindowsNumber(): ?int
+    {
+        return $this->windows_number;
+    }
+
+    public function setWindowsNumber(int $windows_number): self
+    {
+        $this->windows_number = $windows_number;
+
+        return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getOrientation(): ?string
+    {
+        return $this->orientation;
+    }
+
+    public function setOrientation(string $orientation): self
+    {
+        $this->orientation = $orientation;
+
+        return $this;
+    }
+
+    public function getFloor(): ?int
+    {
+        return $this->floor;
+    }
+
+    public function setFloor(int $floor): self
+    {
+        $this->floor = $floor;
 
         return $this;
     }
