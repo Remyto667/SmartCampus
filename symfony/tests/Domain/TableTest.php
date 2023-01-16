6e7e7fa070b69f5da231642e31e5c381068c8908
cbonnard <cyril.bonnard@etudiant.univ-lr.fr>
2023-01-16 10:50:52 +0100

    phpstan

--- symfony/tests/Domain/TableTest.php
+++ symfony/tests/Domain/TableTest.php
@@ -14,12 +14,12 @@ class TableTest extends TestCase
     public function test_room(){
         $type =new Type();
         $type->setName("Salle");
-        $type->setCo2Max("800");
-        $type->setCo2Min("400");
-        $type->setHumMax("60");
-        $type->setHumMin("40");
-        $type->setTempMax("22");
-        $type->setTempMin("19");
+        $type->setCo2Max(800);
+        $type->setCo2Min(400);
+        $type->setHumMax(60);
+        $type->setHumMin(40);
+        $type->setTempMax(22);
+        $type->setTempMin(19);
 
 
         $room=new Room;
@@ -28,27 +28,27 @@ class TableTest extends TestCase
         $room->setIsStock(false);
         $room->setOrientation("N");
         $room->setType($type);
-        $room->setRoomSize("42");
-        $room->setWindowsNumber("5");
+        $room->setRoomSize(42);
+        $room->setWindowsNumber(5);
 
         $this->assertEquals("D280",$room->getName());
         $this->assertEquals(1,$room->getFloor());
         $this->assertEquals("N",$room->getOrientation());
         $this->assertEquals($type,$room->getType());
-        $this->assertEquals("42",$room->getRoomSize());
-        $this->assertEquals("5",$room->getWindowsNumber());
+        $this->assertEquals(42,$room->getRoomSize());
+        $this->assertEquals(5,$room->getWindowsNumber());
     }
 
     public function test_system()
     {
         $type = new Type();
         $type->setName("Salle");
-        $type->setCo2Max("800");
-        $type->setCo2Min("400");
-        $type->setHumMax("60");
-        $type->setHumMin("40");
-        $type->setTempMax("22");
-        $type->setTempMin("19");
+        $type->setCo2Max(800);
+        $type->setCo2Min(400);
+        $type->setHumMax(60);
+        $type->setHumMin(40);
+        $type->setTempMax(22);
+        $type->setTempMin(19);
 
         $room = new Room;
         $room->setName("D280");
@@ -56,16 +56,16 @@ class TableTest extends TestCase
         $room->setIsStock(false);
         $room->setOrientation("N");
         $room->setType($type);
-        $room->setRoomSize("42");
-        $room->setWindowsNumber("5");
+        $room->setRoomSize(42);
+        $room->setWindowsNumber(5);
 
         $system = new System();
         $system->setName("system1");
         $system->setRoom($room);
-        $system->setTag("12");
+        $system->setTag(12);
 
         $this->assertEquals("system1",$system->getName());
-        $this->assertEquals("12",$system->getTag());
+        $this->assertEquals(12,$system->getTag());
         $this->assertEquals($room,$system->getRoom());
     }
 
@@ -73,12 +73,12 @@ class TableTest extends TestCase
     {
         $type = new Type();
         $type->setName("Salle");
-        $type->setCo2Max("800");
-        $type->setCo2Min("400");
-        $type->setHumMax("60");
-        $type->setHumMin("40");
-        $type->setTempMax("22");
-        $type->setTempMin("19");
+        $type->setCo2Max(800);
+        $type->setCo2Min(400);
+        $type->setHumMax(60);
+        $type->setHumMin(40);
+        $type->setTempMax(22);
+        $type->setTempMin(19);
 
         $room = new Room;
         $room->setName("D280");
@@ -86,13 +86,13 @@ class TableTest extends TestCase
         $room->setIsStock(false);
         $room->setOrientation("N");
         $room->setType($type);
-        $room->setRoomSize("42");
-        $room->setWindowsNumber("5");
+        $room->setRoomSize(42);
+        $room->setWindowsNumber(5);
 
         $system = new System();
         $system->setName("system1");
         $system->setRoom($room);
-        $system->setTag("12");
+        $system->setTag(12);
 
         $sensor = new Sensor();
         $sensor->setName("sensor1");
