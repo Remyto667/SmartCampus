7a313e986f7e40ffc43d36ebb77a2d27c816ad56
Remy Riole <remy.riole@etudiant.univ-lr.fr>
2023-01-16 23:24:50 +0100

    Afficher les seuils sur le graphique

--- symfony/src/Controller/AdminController.php
+++ symfony/src/Controller/AdminController.php
@@ -643,8 +643,13 @@ class AdminController extends AbstractController
                  'nb_jours'=>date('t', strtotime($annee . '-' . $month . '-01')),
                 'nb_jours_valide'=>date('t', strtotime($annee . '-' . $month . '-01'))-date("j"),
                 'mois'=>date("m"),
+                'tempMax'=> $room->getType()->getTempMax(),
+                'co2Max'=> $room->getType()->getCo2Max(),
+                'humMax' => $room->getType()->getHumMax(),
+                'tempMin'=> $room->getType()->getTempMin(),
+                'co2Min'=> $room->getType()->getCo2Min(),
+                'humMin' => $room->getType()->getHumMin()
                 ]);
-
         }
     }
 
